386002df71a76910a29bb3c6067b9e98ba39337a
David Colon <dcolonantonio@gmail.com>
2021-05-09 13:55:32 -0400

    removed mocked token

--- migrations/2_deploy_epicbadtiming_token.js
+++
@@ -1,6 +0,0 @@
-const EpicBadTiming = artifacts.require("EpicBadTiming");
-
-module.exports = function (deployer) {
-  // Initialize with 100,000,000 total supply
-  deployer.deploy(EpicBadTiming);
-};
